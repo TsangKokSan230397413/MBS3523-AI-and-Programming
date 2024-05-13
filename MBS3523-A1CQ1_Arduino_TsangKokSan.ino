e4e463a185c44981bde60e94d2db1f84999f7585
TsangKokSan230397413 <159138917+TsangKokSan230397413@users.noreply.github.com>
2024-05-14 00:51:58 +0800

    Add files via upload

---
+++ MBS3523-A1CQ1_Arduino_TsangKokSan.ino
@@ -0,0 +1,24 @@
+#include <DHT.h> 
+ 
+#define DHTPIN 2 
+#define DHTTYPE DHT22 
+ 
+DHT dht(DHTPIN, DHTTYPE); 
+ 
+void setup() { 
+  Serial.begin(9600); 
+  dht.begin(); 
+} 
+ 
+void loop() { 
+  delay(2000); 
+  float temperature = dht.readTemperature(); 
+  if (isnan(temperature)) { 
+    Serial.println("Failed to read from DHT sensor!"); 
+    return; 
+  } 
+
+  Serial.print("Indoor temperature is: "); 
+  Serial.print(temperature); 
+  Serial.println(" degree C"); 
+}
